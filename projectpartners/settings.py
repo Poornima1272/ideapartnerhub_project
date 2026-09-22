09450d8ac3cebfa1786140c7b3a902fcdefa2bba
Poornima1272 <annumishra4432@gmail.com>
2026-09-22 21:21:15 +0530

    Fix database configuration and requirements layout

--- projectpartners/settings.py
+++ projectpartners/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,7 +75,12 @@ WSGI_APPLICATION = "projectpartners.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+if os.environ.get('DATABASE_URL'):
+     DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "partnersdb",
