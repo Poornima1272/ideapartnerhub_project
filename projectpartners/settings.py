22ca0444beb065300a9c2076bc056eaa1302961c
Poornima1272 <annumishra4432@gmail.com>
2026-09-22 21:29:02 +0530

    Fix STATIC_ROOT path configuration for production compile

--- projectpartners/settings.py
+++ projectpartners/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 # Authentication settings
 LOGIN_URL = 'signup'
